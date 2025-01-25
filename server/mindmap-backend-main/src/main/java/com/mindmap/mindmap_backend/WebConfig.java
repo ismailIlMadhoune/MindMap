9d4c71f5266f24b7948daa1c96fea1ee116aa333
ismailIlMadhoune <ilmadhounismail@gmail.com>
2025-01-25 13:55:08 +0000

    Added remote backend URL

--- server/mindmap-backend-main/src/main/java/com/mindmap/mindmap_backend/WebConfig.java
+++ server/mindmap-backend-main/src/main/java/com/mindmap/mindmap_backend/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins(
-                    "http://localhost:5173", 
-                    "https://mind-map-rn.netlify.app"
+                    "http://localhost:5173",    // Localhost URL
+                    "https://mind-map-rn.netlify.app", // Your remote frontend URL
+                    "https://mindmap-backend-44bg.onrender.com" // Remote backend URL (optional, but useful for debugging)
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
